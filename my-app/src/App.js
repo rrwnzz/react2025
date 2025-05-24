c991f751bc989602021540d200f6a354ab0dd2f5
mohammadOthman126 <113715978+rrwnzz@users.noreply.github.com>
2025-05-24 05:43:48 -0700

    style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Nav from './component/nav'
 function App() {
   return (
     <div className="App">
+      <Nav />
     <h1>hi to react</h1>
     </div>
   );
---
+++ my-app/src/component/nav.js
@@ -0,0 +1,12 @@
+import '../css/nav.css'
+export default function Nav () {
+    return (
+        <div id='nave'>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
