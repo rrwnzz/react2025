7c952df2eb563b3837d35878b55330823e11a26b
mohammadOthman126 <113715978+rrwnzz@users.noreply.github.com>
2025-05-28 05:44:05 -0700

    footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import Nav from './component/nav'
+import Nav from './component/nav';
+import Home from './component/home';
+import About from './component/about';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Footer from './component/footer';
 function App() {
-  return (
-    <div className="App">
+  return(
+    <BrowserRouter>
       <Nav />
-    <h1>hi to react</h1>
-    </div>
-  );
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+      </Routes>
+      <Footer />
+      
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- my-app/src/component/footer.js
+++ my-app/src/component/footer.js
@@ -1,9 +1,10 @@
-import '../css/nav.css'
 import {Link} from 'react-router-dom'
-export default function Nav () {
-    return (
-        <div id='nave'>
-            <ul>
+import '../css/footer.css'
+export default function Footer(){
+    return(
+        <div id='footer'>
+            <h1>Footer</h1>
+           <ul>
                 <li>
                     <Link to='/'>Home</Link>
                 </li>
@@ -12,6 +13,9 @@ export default function Nav () {
                 </li>
                 <li>Contact</li>
             </ul>
+            <p id='copy'>copy right @ 2025</p>
         </div>
     )
 }
+
+    
