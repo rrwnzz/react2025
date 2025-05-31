2455974267ef71bbe9cfd939b3a52defd9ec104c
mohammadOthman126 <113715978+rrwnzz@users.noreply.github.com>
2025-05-31 03:58:49 -0700

    added div

--- my-app/src/component/about.js
+++ my-app/src/component/about.js
@@ -1,7 +1,10 @@
 export default function About(){
     return(
-        <h1>
-            About
-        </h1>
+        <div>
+            <h1>
+                About
+            </h1>
+        </div>
+        
     )
 }
--- my-app/src/component/home.js
+++ my-app/src/component/home.js
@@ -1,7 +1,9 @@
 export default function Home (){
     return(
-        <h1>
-            Home page
-        </h1>
+      <div>
+          <h1>
+                Home page
+            </h1>
+      </div>
     )
 }
